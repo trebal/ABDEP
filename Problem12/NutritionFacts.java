a16a44f35d7bf5af32183516f13269ddfd6b8ea1
rdc2 <ramon_dlc@hotmail.com>
2018-05-26 19:12:27 +0200

    Fixed Problem 12

--- Problem12/NutritionFacts.java
+++ Problem12/NutritionFacts.java
@@ -9,14 +9,52 @@ public class NutritionFacts {
     private final int sodium;
     private final int carbohydrate;
 
-    public NutritionFacts(int servingSize, int servings,
-                          int calories, int fat,
-                          int sodium, int carbohydrate) {
+    private NutritionFacts(int servingSize, int servings,
+                           int calories, int fat,
+                           int sodium, int carbohydrate) {
         this.servingSize = servingSize;
         this.servings = servings;
         this.calories = calories;
         this.fat = fat;
         this.sodium = sodium;
         this.carbohydrate = carbohydrate;
+    }
+
+    public static class Builder {
+        private final int servingSize;
+        private final int servings;
+        private int calories;
+        private int fat;
+        private int sodium;
+        private int carbohydrate;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int calories)
+        {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder fat(int fat)
+        {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder sodium(int sodium)
+        {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder carbohydrate(int carbohydrate)
+        {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
     }
 }
